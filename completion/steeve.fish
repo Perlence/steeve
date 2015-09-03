28591d0c1abea8d3596b9330534b94bb54235151
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2015-09-03 22:22:51 +0300

    Ignore output to stderr

--- completion/steeve.fish
+++ completion/steeve.fish
@@ -13,23 +13,26 @@ function __fish_steeve_without_subcommand -d 'Strip all words starting from subc
     set -l cmd (commandline -opc)
     for i in (seq (count $cmd))
         if contains -- $cmd[$i] $subcommands
-            echo $cmd[1..(math $i - 1)]
-            return 0
+            set steeve_without_subcommand $cmd[1..(math $i - 1)]
+            break
         end
     end
-    echo $cmd
+    if [ -z "$steeve_without_subcommand" ]
+        set steeve_without_subcommand $cmd
+    end
+
+    eval "$steeve_without_subcommand $argv 2>/dev/null"
+    set -e steeve_without_subcommand
 end
 
 function __fish_steeve_packages -d 'List steeve packages'
-    set -l steeve_without_subcmd (__fish_steeve_without_subcommand)
-    eval "$steeve_without_subcmd ls -q"
+    __fish_steeve_without_subcommand ls -q
 end
 
 function __fish_steeve_versions -d 'List package versions'
     set -l cmd (commandline -opc)
     set -l package $cmd[-1]
-    set -l steeve_without_subcmd (__fish_steeve_without_subcommand)
-    eval "$steeve_without_subcmd ls -q $package"
+    __fish_steeve_without_subcommand ls -q $package
 end
 
 function __fish_steeve_await_package -a subcommand -d 'Test if steeve has yet to be given a package name'
